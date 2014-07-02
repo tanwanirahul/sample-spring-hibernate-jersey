7b8e7fbfdbefb1be8bd752b741b5558c51cd8d8f
tanwairahul <tanwanirahul@gmail.com>
2014-07-02 14:38:23 +0530

    Adding detail API. This would make availabe /api/tasks/{id}

--- src/main/java/com/hashedin/rest/TaskResource.java
+++ src/main/java/com/hashedin/rest/TaskResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,14 +17,26 @@ import com.hashedin.service.TaskService;
 
 @Component
 @Path("/tasks")
-public class TaskResource {
+public class TaskResource
+{
 
     @Autowired
     private TaskService taskService;
 
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Task> list() {
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public List<Task> list()
+    {
         return taskService.findAll();
+    }
+
+
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @Path("/{taskId}")
+    public Task get(@PathParam("taskId") Long taskId)
+    {
+        return taskService.find(taskId);
     }
 }
