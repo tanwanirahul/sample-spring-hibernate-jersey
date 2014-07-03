8f862fc2e8adcd797998831619489001fc60d873
tanwairahul <tanwanirahul@gmail.com>
2014-07-03 16:13:07 +0530

    Commiting the status code example

--- src/main/java/com/hashedin/rest/TaskResource.java
+++ src/main/java/com/hashedin/rest/TaskResource.java
@@ -1,7 +1,10 @@
 package com.hashedin.rest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,7 +13,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -47,11 +52,12 @@ public class TaskResource
     @POST
     @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Task create(Task task)
+    public Response create(Task task, @Context final HttpServletResponse response) throws URISyntaxException
     {
         // Handles POST on /tasks. Creates a new task and adds it into an repository.
         taskService.save(task);
-        return task;
+        response.setStatus(Response.Status.CREATED.getStatusCode());
+        return Response.created(new URI("/tasks/" + task.getId())).build();
     }
     
     @PUT
